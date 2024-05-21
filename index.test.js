6327da6747eabc79e61ccc12360b61402931baa1
Hannah Phillips <hannah.phillips@simplybusiness.co.uk>
2024-05-21 16:22:17 +0100

    added functionality and tests

--- index.test.js
+++ index.test.js
@@ -7,16 +7,70 @@ const request = require("supertest")
 const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
-const seedMusician = require("./seedData");
-
+const { seedMusician, seedBand } = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+
+    test('GET /musicians', async () => {
+        const response = await request(app).get('/musicians');
+        expect(response.status).toBe(200);
+    });
+
+    test("musicians endpoint", async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        responseData;
+        console.log(responseData[0].name);
+        expect(responseData[0].name).toBe(seedMusician[0].name);
+    });
+
+    test('GET /musicians', async () => {
+        const response = await request(app).get('/musicians');
+        expect(response.status).toBe(200);
+        const responseData = JSON.parse(response.text);
+        console.log(responseData);
+        expect(responseData[0].name).toBe('Mick Jagger');
+        expect(responseData[1].name).toBe('Drake');
+        expect(responseData[2].name).toBe('Jimi Hendrix');
+    });
+
+    test('GET /musicians/:id', async () => {
+        const response = await request(app).get('/musicians/1');
+        expect(response.status).toBe(200);
+        const responseData = JSON.parse(response.text);
+        expect(responseData.name).toBe('Mick Jagger');
+    });
+
+    test('POST /musicians', async () => {
+        const response = await request(app)
+            .post('/musicians')
+            .send({
+                name: 'John Lennon',
+                instrument: 'Guitar'
+            });
+        expect(response.status).toBe(200);
+        const responseData = JSON.parse(response.text);
+        expect(responseData.name).toBe('John Lennon');
+    });
+
+    test('PUT /musicians/:id', async () => {
+        const response = await request(app)
+            .put('/musicians/1')
+            .send({
+                name: 'Elton John',
+                instrument: 'Piano'
+            });
+        expect(response.status).toBe(200);
+        const responseData = JSON.parse(response.text);
+        expect(responseData.name).toBe('Elton John');
+    });
+
+    test('DELETE /musicians/:id', async () => {
+        const response = await request(app).delete('/musicians/4');
+        expect(response.status).toBe(200);
+        const responseData = JSON.parse(response.text);
+        expect(responseData.name).toBe('John Lennon');
+    });
     
-    
-
-
-
-
-    
-})
+});
--- src/app.js
+++ src/app.js
@@ -6,11 +6,35 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.get("/musicians", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+});
 
+app.get("/musicians/:id", async (req, res) => {
+    const musician = await Musician.findByPk(req.params.id);
+    res.json(musician);
+});
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+app.post("/musicians", async (req, res) => {
+    const musician = await Musician.create(req.body);
+    res.json(musician);
+});
 
+app.put("/musicians/:id", async (req, res) => {
+    const musician = await Musician.findByPk(req.params.id);
+    await musician.update(req.body);
+    res.json(musician);
+});
 
-
+app.delete("/musicians/:id", async (req, res) => {
+    const musician = await Musician.findByPk(req.params.id);
+    await musician.destroy();
+    res.json(musician);
+});
 
 module.exports = app;
+
